d0ed39c1d6c7b79904a2fc6dab6b5e717c28271f
Ashraf Eldawody <ashraf6450@gmail.com>
2022-05-02 05:42:36 +0200

    Breadcrumb component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { Fragment, useEffect } from "react";
 import Routes from "./routes/routes";
-import { Windmill } from "@windmill/react-ui";
 import 'flowbite';
 
 import AOS from "aos";
 
 import "./App.scss";
 import Header from "./views/pages/homepage/components/Header";
+import Breadcrumb from "./views/common/SharedComponents/ui/Breadcrumb";
 
 function App() {
   useEffect(() => {
@@ -20,10 +20,9 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Windmill>
+      <Breadcrumb title={"Settings"} parents={[{title:"Account",link:'accounts'}]}></Breadcrumb>
+        <Header />
         <Routes />
-      </Windmill>
     </>
   );
 }
