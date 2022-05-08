8269cb5ac41823e899fda6c637158623d3fc9efc
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-05-08 22:08:57 +0200

    Update ProjectCard to show in both projects and my-projects

--- src/views/pages/projects/components/ProjectCard.tsx
+++ src/views/pages/projects/components/ProjectCard.tsx
@@ -1,33 +1,48 @@
 import React from 'react';
 import CircularProgress from '../../../common/SharedComponents/CircularProgress';
+import moneyFormat from '../../../common/utils/moneyFormat';
 
-type Props = {
-  // projectData: object
+interface projectCardProps {
+  projectData: {
+    id: number;
+    title: string;
+    details: string;
+    images: string[];
+    is_featured: boolean;
+    imageLabel: string;
+    total_target: number;
+    total_donations: number;
+  };
+  projectUrl: string;
+  actionButton: string;
 }
-const ProjectCard: React.FC<Props> = (/* { projectData } */): JSX.Element => {
+
+const ProjectCard: React.FC<projectCardProps> = ({ projectData, projectUrl, actionButton }): JSX.Element => {
+  let donationPercent = projectData.total_donations / projectData.total_target;
+  
   return (
-    <div className="p-3">
+    <div className="p-1">
       <div
         className="app-image-card m-auto max-w-xs rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
         style={{ backgroundImage: "url(https://elementor.zozothemes.com/lendiz/wp-content/uploads/sites/33/2020/01/pro-3-1-527x694.jpg)" }}>
         <div className="app-image-card-back"></div>
-        <div className="p-5 app-image-card-content">
+        <div className="p-1 app-image-card-content">
           <a href="#">
-            <h5 className="mb-2 text-lg font-bold tracking-tight text-white">
-              Noteworthy technology acquisitions 2021
+            <h5 className="mb-2 px-3 text-lg font-bold tracking-tight text-white">
+              { projectData.title }
             </h5>
           </a>
-          <p className="mb-3 text-sm font-normal text-slate-300">
-            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+          <p className="mb-3 px-3 text-sm font-normal text-slate-300">
+            { projectData.details }
           </p>
-          <div className="grid grid-cols-1 md:grid-cols-3 content-center">
-            <CircularProgress progressPercent={0.2} />
+          <div className="grid grid-cols-3 content-center">
+            <CircularProgress progressPercent={donationPercent} />
             <div className="text-white m-auto">
               <div>TARGET</div>
-              <div>$15,000</div>
+              <div>{ moneyFormat(projectData.total_target) }</div>
             </div>
-            <a href="#" className="m-auto text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-1 py-2.5 text-center">
-              Donate
+            <a href={projectUrl} className="m-auto text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 py-2.5 text-center">
+              { actionButton }
             </a>
           </div>
         </div>
