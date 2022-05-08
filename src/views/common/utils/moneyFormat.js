c82220013e9be0b7a24da30bece76705838c8cde
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-05-08 22:08:03 +0200

    Set fixed number to zero in moneyFormat

--- src/views/common/utils/moneyFormat.js
+++ src/views/common/utils/moneyFormat.js
@@ -1,4 +1,4 @@
 const moneyFormat =  (num) => {
-    return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    return '$' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
 export default moneyFormat;
