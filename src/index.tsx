d9ff1d3919371d5441cdcf5f4d978767142b2bfc
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-05-09 05:04:45 +0200

    Add multi select and date range picker packages

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import {Provider} from "react-redux";
 import store from "./app/store";
 import { BrowserRouter } from "react-router-dom";
 import 'tw-elements';
+import 'flowbite'
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
