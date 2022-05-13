7d2cf64083428df9ed0fe958363458b7c84b8a96
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-05-13 22:37:55 +0200

    Add New Project button in My Projects page

--- src/views/pages/projects/components/ProjectsGrid.tsx
+++ src/views/pages/projects/components/ProjectsGrid.tsx
@@ -1,14 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate} from "react-router-dom";
-import useProjectsApi from "../../../../services/hooks/useProjectsApi";
+import { Link } from 'react-router-dom';
 import ProjectCard from "./ProjectCard";
 
-const ProjectsGrid: React.FC<any> = ({title,projects}): JSX.Element => {
-
-    return  (
+const ProjectsGrid: React.FC<any> = ({ title, projects }): JSX.Element => {
+    return (
         <div className="block p-6 text-left w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 className="mb-2 self-start text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-          <hr/>
+            <div className="flex">
+                <h5 className="grow mb-2 self-start text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                <Link to={"/my-projects/new"} type="button" className="mb-2 grow-0 inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">
+                    New Project
+                </Link>
+            </div>
+            <hr />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 place-content-evenly mt-8">
 
                 {projects.length > 0 && projects.map((project: any) => (
