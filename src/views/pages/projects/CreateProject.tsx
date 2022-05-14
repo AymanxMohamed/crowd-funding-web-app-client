6bd311b9dcf4315c11f068980a353b9086ede830
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-05-14 09:22:55 +0200

    Fix projects images upload

--- src/views/pages/projects/CreateProject.tsx
+++ src/views/pages/projects/CreateProject.tsx
@@ -248,7 +248,7 @@ const CreateProject: React.FC = (): JSX.Element => {
                           <div className="mb-3 w-96">
                             <label htmlFor="formFileMultiple" className="form-label inline-block mb-2 text-gray-700">Choose Project Images</label>
                             <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                              type="file" id="formFileMultiple" multiple />
+                              type="file" name="images" id="formFileMultiple" multiple />
                           </div>
                         </div>
 
